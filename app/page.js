07ef3554f5dc3eb3f601129865cac7c48296c2c6
hafizmrifqi <hafizrifqi1303@gmail.com>
2025-02-12 09:27:29 +0700

    Adjust Footer

--- app/page.js
+++ app/page.js
@@ -31,28 +31,47 @@ export default function Home() {
   };
 
   return (
-    <div className="bg-white p-8 rounded-lg shadow-lg">
-      <h1 className="text-2xl font-bold mb-4">QR Code Generator</h1>
-      <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Masukkan teks atau URL" className="border p-2 w-full mb-4 rounded" />
-      <button onClick={generateQrCode} className="bg-blue-500 text-white p-2 rounded mb-4 mx-auto">
-        Generate
-      </button>
-      <div className="mb-4">
-        {qrCode && (
-          <QRCodeCanvas
-            value={qrCode}
-            size={256}
-            level="H"
-            className="mx-auto"
-            ref={qrRef} // Set ref ke komponen QRCodeCanvas
-          />
+    <div>
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold mb-4">QR Code Generator</h1>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Masukkan teks atau URL" className="border p-2 w-full mb-4 rounded" />
+        <button onClick={generateQrCode} className="bg-blue-500 text-white p-2 rounded mb-4 mx-auto">
+          Generate
+        </button>
+        <div className="mb-4">
+          {qrCode && (
+            <QRCodeCanvas
+              value={qrCode}
+              size={256}
+              level="H"
+              className="mx-auto"
+              ref={qrRef} // Set ref ke komponen QRCodeCanvas
+            />
+          )}
+        </div>
+        {qrCode && ( // Tampilkan tombol download hanya jika QR code ada
+          <button onClick={downloadQRCode} className="bg-green-500 text-white p-2 rounded mx-auto">
+            Download QR Code
+          </button>
         )}
       </div>
-      {qrCode && ( // Tampilkan tombol download hanya jika QR code ada
-        <button onClick={downloadQRCode} className="bg-green-500 text-white p-2 rounded mx-auto">
-          Download QR Code
-        </button>
-      )}
+
+      <footer className="fixed bottom-0 left-0 px-4 bg-gray-100 py-4 text-center text-gray-700 border-t border-gray-200">
+        <div className="container mx-auto">
+          {" "}
+          {/* Center content */}
+          &copy; {new Date().getFullYear()} QR Boat. Created with ❤️ by{" "}
+          <a
+            href="https://github.com/hafizmrifqi"
+            className="font-medium text-blue-600 hover:underline" // Style the link
+            target="_blank" // Open in new tab (important for external links)
+            rel="noopener noreferrer" // Security best practice
+          >
+            hafizmrifqi
+          </a>
+          . All rights reserved. {/* Added a more complete sentence */}
+        </div>
+      </footer>
     </div>
   );
 }
